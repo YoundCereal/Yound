5cc2c420a1eb41b378b8677255dcaf7f546e1d95
YoundCereal <53799766+YoundCereal@users.noreply.github.com>
2019-08-05 20:02:52 -0400

    Delete Mad Libs.py

--- Mad Libs.py
+++
@@ -1,8 +0,0 @@
-number_1 = input("Put in an Adjective: ")
-number_2 = input("Put in a Noun Now: ")
-number_3 = input("Now a Verb: ")
-result = "The " + number_1 + " " + number_2 + " was " + number_3 + "ing" + "."
-
-print(result)
-print(" ")
-input("Press Enter to Exit the Program")
